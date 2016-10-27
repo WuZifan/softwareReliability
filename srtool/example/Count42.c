fd6e1e730281e3ff7980cb1f989d2ad5bec5c779
w460461339 <wuzifan0817@gmail.com>
2016-10-27 17:38:46 +0100

    fix pull problem

--- srtool/example/Count42.c
+++ srtool/example/Count42.c
@@ -6,13 +6,15 @@ int foo(int a, int b)
 {
 	int j;
 	int i;
-	assert(1==1);
+	i=501;
+// 	assert(1==1);
 	j=1;
-	if (i == 0) {
-		j = 2;
-	}
-	else {
-		i = 1;
-	}
+	i=i/(j-j);
+// 	if (i == 0) {
+// 		j = 2;
+// 	}
+// 	else {
+// 		i = 1;
+// 	}
 	return 0;
 }
