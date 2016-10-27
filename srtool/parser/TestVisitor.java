c6eae6445a9b892d5b58fa7833256b5edbb0eea4
w460461339 <wuzifan0817@gmail.com>
2016-10-27 12:53:47 +0100

    assert order

--- srtool/parser/TestVisitor.java
+++ srtool/parser/TestVisitor.java
@@ -153,6 +153,7 @@ public class TestVisitor extends SimpleCBaseVisitor<String> {
 					tempSmt += " (ite " + cond + " " + key + Integer.toString(afif.get(key).get(1));
 					tempSmt += " " + key + Integer.toString(this.variCount.get(key).get(1)) + "))\n";
 					incSubscript(key);
+					incSubscript(key);
 					System.out.println("tmp res : " + tempSmt.toString());
 				}
 				else if(afif.get(key).get(1) < this.variCount.get(key).get(1)) {
@@ -160,12 +161,14 @@ public class TestVisitor extends SimpleCBaseVisitor<String> {
 					tempSmt += " (ite " + cond + " " + key + Integer.toString(this.variCount.get(key).get(1));
 					tempSmt += " " + key + Integer.toString(afif.get(key).get(1)) + "))\n";
 					incSubscript(key);
+					incSubscript(key);
 					System.out.println("tmp res : " + tempSmt.toString());
 				}
 				else if(afif.get(key).get(1) > init.get(key).get(1)) {
 					tempSmt += "(assert (= " + key + Integer.toString(this.variCount.get(key).get(1) + 1);
 					tempSmt += " (ite " + cond + " " + key + Integer.toString(afif.get(key).get(1));
 					tempSmt += " " + key + Integer.toString(init.get(key).get(1)) + "))\n";
+					incSubscript(key);
 					incSubscript(key);
 				}
 				
